edbe28004889b73c97ec188cbd1639c1cffaf66a
Injector-Spenral <sam.richards@thedropbears.org.au>
2019-01-20 13:16:03 +1100

    Added ground tape code

--- cameraServer.py
+++ cameraServer.py
@@ -6,11 +6,15 @@ import numpy as np
 import cv2
 import math
 
+from cscore import CameraServer, VideoSource
+from networktables import NetworkTables
+    
+
 #Magic Numbers
 lowerGreen = (38, 125, 100) #Our Robot's Camera
 higherGreen = (52, 255, 180)
-sample_lowerGreen = (30, 177, 80) #FRC sample images
-sample_higherGreen = (150, 255, 255)
+sampleLowerGreen = (30, 177, 80) #FRC sample images
+sampleHigherGreen = (150, 255, 255)
 lowerTapeColour = (0, 0, 0) #TODO: Ground Tape filter colours
 upperTapeColour = (360, 255, 255) #TODO: Ground Tape filter colours 
 minContourArea = 10
@@ -18,6 +22,8 @@ angleOffset = 13
 rightAngleSize = -14.5
 leftAngleSize = -75.5
 screenSize = (320, 240)
+
+
 
 #Initialisation
 configFile = "/boot/frc.json"
@@ -28,8 +34,8 @@ team = None
 server = False
 cameraConfigs = []
 
-"""Read single camera configuration."""
 def readCameraConfig(config):
+    """Read single camera configuration."""
     cam = CameraConfig()
 
     # name
@@ -43,7 +49,7 @@ def readCameraConfig(config):
     try:
         cam.path = config["path"]
     except KeyError:
-        parseError("camera '{}': could not read path".format(cam.name))
+        parseError("camera '" + str(cam.name) + "': could not read path")
         return False
 
     cam.config = config
@@ -51,12 +57,12 @@ def readCameraConfig(config):
     cameraConfigs.append(cam)
     return True
 
-"""Report parse error."""
-def parseError(str):
-    print("config error in '" + configFile + "': " + str, file=sys.stderr)
+def parseError(error_str):
+    """Report parse error."""
+    print("config error in '" + configFile + "': " + error_str, file=sys.stderr)
 
-"""Read configuration file."""
 def readConfig():
+    """Read configuration file."""
     global team
     global server
 
@@ -65,7 +71,7 @@ def readConfig():
         with open(configFile, "rt") as f:
             j = json.load(f)
     except OSError as err:
-        print("could not open '{}': {}".format(configFile, err), file=sys.stderr)
+        print("could not open '" + str(configFile) + "': " + str(err), file=sys.stderr)
         return False
 
     # top level must be an object
@@ -82,13 +88,13 @@ def readConfig():
 
     # ntmode (optional)
     if "ntmode" in j:
-        str = j["ntmode"]
-        if str.lower() == "client":
+        nt_str = j["ntmode"]
+        if nt_str.lower() == "client":
             server = False
-        elif str.lower() == "server":
+        elif nt_str.lower() == "server":
             server = True
         else:
-            parseError("could not understand ntmode value '{}'".format(str))
+            parseError("could not understand ntmode value '" + str(nt_str))
 
     # cameras
     try:
@@ -102,9 +108,11 @@ def readConfig():
 
     return True
 
-"""Start running the camera."""
+
+
+#Our code begins here
 def startCamera(config):
-    print("Starting camera '{}' on {}".format(config.name, config.path))
+    """Start running the camera."""
     cs = CameraServer.getInstance()
     camera = cs.startAutomaticCapture(name=config.name, path=config.path)
     camera.setConfigJson(json.dumps(config.config))
@@ -113,12 +121,11 @@ def startCamera(config):
 
 
 #Process Functions
-
 def getRetroPos(img, display=False, sample=False):
     """Function for finding retro-reflective tape a"""
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) #Convert to HSV to make the mask easier
     if sample:
-        mask = cv2.inRange(hsv, sample_lowerGreen, sample_higherGreen) #Create a mask of everything in between the greens
+        mask = cv2.inRange(hsv, sampleLowerGreen, sampleHigherGreen) #Create a mask of everything in between the greens
     else:
         mask = cv2.inRange(hsv, lowerGreen, higherGreen)
     mask = cv2.dilate(mask, None, iterations=2) #Expand the mask to allow for further away tape
@@ -145,7 +152,7 @@ def getRetroPos(img, display=False, sample=False):
         if len(pairs) >= 1:
             closestToMiddle = min(pairs, key = lambda x:abs(x[0][0][0] - screenSize[0]/2))
         else:
-            return None, None, img
+            return float("inf"), float("inf"), img
 
         circleContours = list(np.int0(cv2.boxPoints(closestToMiddle[0])))
         circleContours.extend(list(np.int0(cv2.boxPoints(closestToMiddle[1]))))
@@ -156,7 +163,7 @@ def getRetroPos(img, display=False, sample=False):
 
         angle = (closestToMiddle[0][1][0] * closestToMiddle[0][1][1]) / (closestToMiddle[1][1][0] * closestToMiddle[1][1][1])
         
-        if display == True: #Create the annotated display if display is True
+        if display: #Create the annotated display if display is True
             for pair in pairs:
                 if pair == closestToMiddle:
                     for tape in pair:
@@ -167,16 +174,73 @@ def getRetroPos(img, display=False, sample=False):
                         img = cv2.drawContours(img, [np.int0(cv2.boxPoints(tape))], 0, (0, 0, 255))
 
             return angle, -(((x/screenSize[0])*2)-1), img
-    return None, None, img
+    return float("inf"), float("inf"), img
 
 def getGroundPos(img, sample=False): #TODO: Ground Tape Function
-    return None, None, None #Distance from x, distance from y, angle of line
-    
-#if False:
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    mask = cv2.inRange(gray, (50), (255))
+
+    contours = cv2.findContours(mask, 1, 2)[-2]
+
+    if len(contours) >= 1:
+        contours.sort(key=cv2.contourArea)
+        cnt = contours[-1]
+        if cv2.contourArea(cnt) > 100:
+            rect = cv2.minAreaRect(cnt)
+            boxpoints = np.int0(cv2.boxPoints(rect))
+            img = cv2.drawContours(img, [boxpoints], 0, (255))
+
+            inner_points = []
+            for point in boxpoints:
+                outside = False
+                for i in range(2):
+                    if not 5 < point[i] < gray.shape[not i] - 5:
+                        outside = True
+                if not outside:
+                    inner_points.append(point)
+                
+            
+            #Cases:
+            if len(inner_points) == 2: #If 2 points are on the screen, give their average
+                avrPoints = np.average(inner_points, axis=0)
+                x_float = -((avrPoints[0] / (gray.shape[1]/2))-1)
+                y_float = -((avrPoints[1] / (gray.shape[0]/2))-1)
+                orderedPoints = sorted(inner_points, key=lambda x:x[0])
+                distance_x = orderedPoints[1][0] - orderedPoints[0][0]
+                distance_y = orderedPoints[1][1] - orderedPoints[0][1]
+                angle = -math.atan2(distance_y, distance_x)
+                return angle, x_float, y_float
+            elif len(inner_points) == 1: #If one point is on the screen, return its location
+                x_float = -((inner_points[0][0] / (gray.shape[1]/2))-1)
+                y_float = -((inner_points[0][1] / (gray.shape[0]/2))-1)
+                return 0, x_float, y_float
+            elif len(inner_points) == 0:
+                avrPoints = np.average(boxpoints, axis=0)
+                x_float = -((avrPoints[0] / (gray.shape[1]/2))-1)
+                y_float = -((avrPoints[1] / (gray.shape[0]/2))-1)
+                distance_1 = math.sqrt((boxpoints[0][0] - boxpoints[1][0])**2 + (boxpoints[0][1] - boxpoints[1][1]))
+                distance_2 = math.sqrt((boxpoints[0][0] - boxpoints[2][0])**2 + (boxpoints[0][1] - boxpoints[2][1]))
+                if distance_1 < distance_2:
+                    distance_x = boxpoints[1][0] - boxpoints[0][0]
+                    distance_y = boxpoints[1][1] - boxpoints[0][1]
+                else:
+                    distance_x = boxpoints[2][0] - boxpoints[0][0]
+                    distance_y = boxpoints[2][1] - boxpoints[0][1]
+                angle = -math.atan2(distance_y, distance_x)
+                return angle, x_float, y_float
+            else:
+                avrPoints = np.average(inner_points, axis=0)
+                x_float = -((avrPoints[0] / (gray.shape[1]/2))-1)
+                y_float = -((avrPoints[1] / (gray.shape[0]/2))-1)
+                return len(inner_points), x_float, y_float
+            
+            
+            #Distance from x, distance from y, angle of line
+    return float("inf"), float("inf"), float("inf")
+ 
+
+
 if __name__ == "__main__":
-    from cscore import CameraServer, VideoSource
-    from networktables import NetworkTables
-    
     if len(sys.argv) >= 2:
         configFile = sys.argv[1]
 
@@ -189,27 +253,33 @@ if __name__ == "__main__":
 
     
     nt = NetworkTables.getTable('/vision')
-    entry = nt.getEntry('target_tape_error')
-    entry_angle = nt.getEntry('angle')
+    entry_x = nt.getEntry('ground_tape_x')
+    entry_angle = nt.getEntry('ground_tape_angle')
+    entry_y = nt.getEntry('ground_tape_y')
+    entry_tape_angle = nt.getEntry('target_tape_error')
+    entry_wall_ratio = nt.getEntry('wall_angle')
 
     # start cameras
     cameras = []
     for cameraConfig in cameraConfigs:
         cameras.append(startCamera(cameraConfig))
 
-    sink = cameras[0][0].getVideo(camera=cameras[0][1])
+    ground_sink = cameras[0][0].getVideo(camera=cameras[0][1])
+    retro_sink = cameras[1][0].getVideo(camera=cameras[1][1])
     source = cameras[0][0].putVideo('cv', 320, 240)
 
-    frame = np.zeros(shape=(screenSize[1], screenSize[0], 3))
+    ground_frame = np.zeros(shape=(screenSize[1], screenSize[0], 3))
+    retro_frame = np.zeros(shape=(screenSize[1], screenSize[0], 3))
     while True:
-        time1, frame = sink.grabFrameNoTimeout(image=frame)
-        angle, percent, image = getRetroPos(frame, True)
+        time1, ground_frame = ground_sink.grabFrameNoTimeout(image=ground_frame)
+        time2, retro_frame = retro_sink.grabFrameNoTimeout(image=retro_frame)
+        retro_angle, percent, image = getRetroPos(retro_frame, True)
+        ground_angle, x, y = getGroundPos(ground_frame)
 
-        if not percent:
-            percent = 999
-        if not angle:
-            angle = 999
         source.putFrame(image)
-        entry.setNumber(percent)
-        entry_angle.setNumber(angle)
+        entry_x.setNumber(x)
+        entry_y.setNumber(y)
+        entry_angle.setNumber(ground_angle)
+        entry_tape_angle.setNumber(percent)
+        entry_wall_ratio.setNumber(retro_angle)
         NetworkTables.flush()
