3a8afb29bdf43a87dccdc53766694d067cf7fb03
Injector-Spenral <sam.richards@thedropbears.org.au>
2019-03-12 14:50:58 +1100

    Added new tests

--- tests.py
+++ tests.py
@@ -1,24 +1,51 @@
 from cameraServer import getRetroPos
 import cv2
 import numpy as np
+import math
+import csv
+import sys
 
-screenSize = (320, 240)
 
-images = [
-    ["350mm.png", (0.33, 0.37), (-0.05, 0.05)],
-    ["530mm.png", (0.5, 0.56), (-0.05, 0.05)],
-    ["740mm.png", (0.71, 0.77), (-0.05, 0.05)],
-    ["830-300mm.png", (0.8, 0.86), (-0.3, -0.2)],
-    ["860-90mm.png", (0.83, 0.89), (-0.15, -0.05)],
-    ["1210mm.png", (1.16, 1.26), (-0.1, 0.1)],
-]
+def test_images():
+    acceptable_error = 0.08
+    with open("samples/target.csv", "r") as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=",")
+        csvlist = list(csvreader)
+        for row in csvlist:
+            if row:
+                frame = cv2.imread("samples/" + row[0])
+                result = getRetroPos(frame, True, hsv, mask)
+                data = (float(row[1]), float(row[2]))  # Distance, Offset
+                if math.isnan(result[1]):
+                    assert math.isnan(data[0]) and math.isnan(result[1])
+                    assert math.isnan(data[1]) and math.isnan(result[2])
+                    continue
+                assert (
+                    data[0] - acceptable_error < result[1] < data[0] + acceptable_error
+                )
+                assert (
+                    data[1] - acceptable_error < result[2] < data[1] + acceptable_error
+                )
 
-hsv = np.zeros(shape=(screenSize[1], screenSize[0], 3), dtype=np.uint8)
-mask = np.zeros(shape=(screenSize[1], screenSize[0]), dtype=np.uint8)
 
-for image in images:
-    values = getRetroPos(cv2.imread('.\\samples\\' + image[0]), True, hsv, mask)
-    print(values[1], values[2])
-    assert image[1][0] < values[1] < image[1][1]
-    assert image[2][0] < values[2] < image[2][1]
-print("tests passed")
+def test_video(path):
+    cap = cv2.VideoCapture(path)
+    if cap.isOpened() == False:
+        print("Error opening video stream or file", file=sys.stderr)
+        sys.exit()
+    while True:
+        ret, frame = cap.read()
+        result = getRetroPos(frame, True, hsv, mask)
+        if ret:
+            cv2.imshow("frame", result[0])
+            print(result[1], result[2])
+            cv2.waitKey(0)
+        else:
+            break
+
+if __name__ == "__main__":
+    screenSize = (320, 240)
+    hsv = np.zeros(shape=(screenSize[1], screenSize[0], 3), dtype=np.uint8)
+    mask = np.zeros(shape=(screenSize[1], screenSize[0]), dtype=np.uint8)
+    test_images()
+
